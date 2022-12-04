f69cadffab188e639be4f89afc8c144ed1da67a1
devops0014 <106308110+devops0014@users.noreply.github.com>
2022-12-04 16:29:46 +0530

    Delete Terraform directory

--- Terraform/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "dev_server" {
-ami = var.ami_id
-instance_type = var.instance_type
-key_name = var.key_name
-tags = {
-Name = "${var.name}"
-Environment = "dev"
-}
-}
--- Terraform/provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-region = "ap-south-1"
-access_key = "AKIA4QUKASHDUCIVCMXL"
-secret_key = "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"
-}
